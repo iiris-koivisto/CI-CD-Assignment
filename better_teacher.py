f1f0b5c47d6ca068f89d7f4db056337a44fb0aa0
Siri Pulli <sirppesipuli94@outlook.com>
2023-04-17 15:00:53 +0300

    created better_teacher.py (hasn't been tested yet)

---
+++ better_teacher.py
@@ -0,0 +1,10 @@
+better_teacher = input("Who is the better teacher? Answer C-A-R-E-F-U-L-L-Y: ")
+
+if better_teacher == 'Sanna':
+    print("Sanna da best! # Sori Marika! We lub u!")
+
+elif better_teacher == 'Marika':
+    print("Marika da best! # Sori Sanna! We lub u.... uwu")
+
+else:
+    print("ARE YOU DRUNK BRO?!")
