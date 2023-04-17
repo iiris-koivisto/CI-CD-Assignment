f5fecd343ae3a33ed3eb35ef0c68d309f2b2fa33
iiris-koivisto <91025408+iiris-koivisto@users.noreply.github.com>
2023-04-17 10:31:58 +0300

    Created main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+km = 12.25
+mi = 7.38
+
+mi_to_km = mi * 1.61
+km_to_mi = km / 1.61
+
+print(mi, "miles is", round(mi_to_km, 2), "kilometers")
+print(km, "kilometers is", round(km_to_mi, 2), "miles")
